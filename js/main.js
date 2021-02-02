e61a7e937c055a9edf44b2633bf7d57601d0bd79
parkh <heavenp2331@gmail.com>
2021-02-01 19:26:53 -0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,136 @@
+
+$(document).ready(function(){
+    
+    $('.primary').click(function(event){
+        console.log('Clicked Button')
+        
+        var subject = $('.subject').val()
+        var email = $('.email').val()
+        var message = $('.message').val()
+        var statusElm = $('.status')
+        statusElm.empty()
+        
+        if(email.length > 5 && email.includes('@') && email.includes('.')){
+            
+            statusElm.append('<div>Email is valid</div>')
+        }else {
+            event.preventDefault()
+            statusElm.append('<div>Email is invalid</div>')
+        }
+        if(subject.length >- 2){
+            statusElm.append('<div>Subject is valid</div>')
+        }else{
+            event.preventDefault()
+            statusElm.append('<div>Subject is invalid</div>')
+        }
+        if(message.length >= 10){
+            statusElm.append('<div>Message is valid</div>')
+        }else{
+            event.preventDefault()
+            statusElm.append('<div>Message is invalid</div>')
+        }
+    })
+    
+})
+
+
+$(document).ready(function() {
+		$('a[href*=#]').bind('click', function(e) {
+				e.preventDefault(); // prevent hard jump, the default behavior
+
+				var target = $(this).attr("href"); // Set the target as variable
+
+				// perform animated scrolling by getting top-position of target-element and set it as scroll target
+				$('html, body').stop().animate({
+						scrollTop: $(target).offset().top
+				}, 600, function() {
+						location.hash = target; //attach the hash (#jumptarget) to the pageurl
+				});
+
+				return false;
+		});
+});
+
+$(window).scroll(function() {
+		var scrollDistance = $(window).scrollTop();
+
+		// Show/hide menu on scroll
+		//if (scrollDistance >= 850) {
+		//		$('nav').fadeIn("fast");
+		//} else {
+		//		$('nav').fadeOut("fast");
+		//}
+	
+		// Assign active class to nav links while scolling
+		$('.page-section').each(function(i) {
+				if ($(this).position().top <= scrollDistance) {
+						$('.navigation a.active').removeClass('active');
+						$('.navigation a').eq(i).addClass('active');
+				}
+		});
+}).scroll();
+
+
+$(document).ready(function () {
+
+
+    let $btns = $('.project-area .button-group button');
+
+
+    $btns.click(function (e) {
+
+        $('.project-area .button-group button').removeClass('active');
+        e.target.classList.add('active');
+
+        let selector = $(e.target).attr('data-filter');
+        $('.project-area .grid').isotope({
+            filter: selector
+        });
+
+        return false;
+    })
+
+    $('.project-area .button-group #btn1').trigger('click');
+
+    $('.project-area .grid .test-popup-link').magnificPopup({
+        type: 'image',
+        gallery: { enabled: true }
+    });
+
+
+    // Owl-carousel
+
+    $('.site-main .about-area .owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            560: {
+                items: 2
+            }
+        }
+    })
+
+    // sticky navigation menu
+
+    let nav_offset_top = $('.header_area').height() + 50;
+
+    function navbarFixed() {
+        if ($('.header_area').length) {
+            $(window).scroll(function () {
+                let scroll = $(window).scrollTop();
+                if (scroll >= nav_offset_top) {
+                    $('.header_area .main-menu').addClass('navbar_fixed');
+                } else {
+                    $('.header_area .main-menu').removeClass('navbar_fixed');
+                }
+            })
+        }
+    }
+
+    navbarFixed();
+
+});
